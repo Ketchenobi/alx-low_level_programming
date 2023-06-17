f22ca22ebeac176622be630c8ce155b8fd44e946
ketchenobi <ketchenobimayuk@gmail.com>
2023-06-17 16:07:37 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main. A program that prints an output with a puts function
+ * return: (0) (Success)
+ */
+int main(void)
+{
+	/* using the puts function*/
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

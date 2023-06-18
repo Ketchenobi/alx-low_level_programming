51249d70c5698399ffc93ce4aae652f7a3565a7c
ketchenobi <ketchenobimayuk@gmail.com>
2023-06-17 17:09:15 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 /**
- * main - A program that prints an output with a puts function
+ * main -main entry point
+ * A program that prints an output with a puts function
+ * The main function of the program.
+ *
+ * @return 0 on successful execution
  * return: (0) (Success)
+ *  @return 0 on success
  */
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints an output with a puts function
+ * The main function of the program.
+ *
+ * @return 0 on successful execution
+ * return: (0) (Success)
+ *  @return 0 on success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}

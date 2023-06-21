4ef6058ec1b7353ac21d973c1ab392ee5c73d976
ketchenobi <ketchenobimayuk@gmail.com>
2023-06-21 10:53:27 -0700

    task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
-#include<main.h>
+#include "main.h"
+
 /**
- * main - Entry point
- * Return: Always 0
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
  */
-Int main(void)
+int main(void)
 {
 int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 int count, sz;
@@ -15,3 +17,4 @@ _putchar(str[count]);
 _putchar('\n');
 return (0);
 }
+
